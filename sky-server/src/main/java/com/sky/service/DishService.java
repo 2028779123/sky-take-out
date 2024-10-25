feeb54b4e1dbb8cae3d18b7d1cc701637604a204
owl <z2028779123@gmail.com>
2024-10-25 16:44:18 +0800

    新增菜品

---
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -0,0 +1,33 @@
+package com.sky.service;
+
+import com.sky.dto.DishDTO;
+
+public interface DishService {
+
+
+    /**
+     * 新增菜品
+     * @param dishDTO
+     */
+    public void savaWithFlavor(DishDTO dishDTO);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
