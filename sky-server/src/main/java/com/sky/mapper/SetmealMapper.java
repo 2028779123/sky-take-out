db6d4f308aede2e430632e0b634ebe4eb11c2f67
owl <z2028779123@gmail.com>
2024-10-31 23:42:20 +0800

    新增套餐

--- sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealMapper.java
@@ -1,10 +1,21 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Setmeal;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface SetmealMapper {
+
+    /**
+     * 插入套餐数据
+     * @param setmeal
+     */
+
+    @AutoFill(OperationType.INSERT)
+    void insert(Setmeal setmeal);
 
     /**
      * 根据分类id查询套餐的数量
@@ -13,5 +24,4 @@ public interface SetmealMapper {
      */
     @Select("select count(id) from setmeal where category_id = #{categoryId}")
     Integer countByCategoryId(Long id);
-
 }
---
+++ sky-server/src/main/java/com/sky/service/impl/SetmealServiceImpl.java
@@ -0,0 +1,51 @@
+package com.sky.service.impl;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.mapper.SetmealMapper;
+import com.sky.mapper.SetmealDishMapper;
+import com.sky.service.SetmealService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Slf4j
+public class SetmealServiceImpl implements SetmealService {
+    @Autowired
+    private SetmealMapper setmealMapper;
+
+    @Autowired
+    private SetmealDishMapper setmealDishMapper;
+    @Transactional
+    public void saveWithDish(SetmealDTO setmealDTO) {
+        Setmeal setmeal = new Setmeal();
+        BeanUtils.copyProperties(setmealDTO,setmeal);
+        //向套餐表中插入数据
+        setmealMapper.insert(setmeal);
+        //获取insert语句生成的主键值，通过keyProperty传递回来
+        Long setmealId = setmeal.getId();
+
+        List<SetmealDish> setmealDishes = setmealDTO.getSetmealDishes();
+        //向setmeal_dish表中插入n条dish数据
+        if(setmealDishes != null&&setmealDishes.size()>0){
+            setmealDishes.forEach(setmealDish -> {
+                setmealDish.setSetmealId(setmealId);
+            });
+
+            //批量插入菜品数据
+            setmealDishMapper.insertBatch(setmealDishes);
+
+        }
+
+
+
+
+
+    }
+}
